0cf2a0071fddce98f12a89a31847b4c375b6a6a7
Sergei <szachesov@yandex.ru>
2022-04-17 18:35:46 +0300

    Update schema DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,20 @@
-INSERT INTO restoration (name)
-VALUES ('12 Стульев'),
-       ('Макдональдс');
-
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', '{noop}password'),
        ('Admin', 'admin@gmail.com', '{noop}admin');
 
 INSERT INTO user_roles (role, user_id)
-VALUES ('USER', 100003),
-       ('ADMIN', 100003),
-       ('USER', 100002);
+VALUES ('USER', 100001),
+       ('ADMIN', 100001),
+       ('USER', 100000);
 
-INSERT INTO dish(restoration_id, name, price)
-VALUES (100000, 'Жульен', 100),
-       (100001, 'Борщ', 200)
+INSERT INTO restoration (name)
+VALUES ('12 Стульев'),
+       ('Макдональдс');
 
+INSERT INTO dish(name, price)
+VALUES ('Жульен', 100),
+       ('Борщ', 200);
 
+INSERT INTO menu (date_menu, restoration_id, dish_id)
+VALUES ('2022-04-01', 100002, 100004),
+       ('2022-04-01', 100003, 100005);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,21 @@
 DROP TABLE user_roles IF EXISTS;
-DROP TABLE dish IF EXISTS;
+DROP TABLE voice IF EXISTS;
 DROP TABLE users IF EXISTS;
+DROP TABLE dish IF EXISTS;
+DROP TABLE menu IF EXISTS;
 DROP TABLE restoration IF EXISTS;
+
 DROP SEQUENCE global_seq IF EXISTS;
 
 CREATE SEQUENCE global_seq START WITH 100000 INCREMENT BY 1;
 
-CREATE TABLE restoration
-(
-    id   INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE users
 (
-    id             INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
-    name           VARCHAR(255) NOT NULL,
-    email          VARCHAR(255) NOT NULL,
-    password       VARCHAR(255) NOT NULL,
-    restoration_id INTEGER,
-    FOREIGN KEY (restoration_id) REFERENCES restoration (id)
+    id       INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
-
-CREATE UNIQUE INDEX users_unique_email_idx ON USERS (email);
 
 CREATE TABLE user_roles
 (
@@ -32,12 +25,37 @@ CREATE TABLE user_roles
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
+CREATE TABLE restoration
+(
+    id   INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE dish
 (
+    id    INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
+    name  VARCHAR(255) NOT NULL,
+    price DOUBLE       NOT NULL
+);
+
+CREATE TABLE menu
+(
     id             INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
-    restoration_id INTEGER      NOT NULL,
-    name           VARCHAR(255) NOT NULL,
-    price          DOUBLE       NOT NULL,
-    CONSTRAINT restoration_dish_id UNIQUE (restoration_id, id),
-    FOREIGN KEY (restoration_id) REFERENCES restoration (id) ON DELETE CASCADE
-)
+    date_menu      DATE    NOT NULL,
+    restoration_id INTEGER NOT NULL,
+    dish_id        INTEGER NOT NULL,
+    FOREIGN KEY (restoration_id) REFERENCES restoration (id) ON DELETE CASCADE,
+    FOREIGN KEY (dish_id) REFERENCES dish (id) ON DELETE CASCADE,
+    CONSTRAINT restoration_dish_date UNIQUE (restoration_id, dish_id, date_menu)
+);
+
+CREATE TABLE voice
+(
+    id             INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
+    user_id        INTEGER   NOT NULL,
+    restoration_id INTEGER   NOT NULL,
+    date_time      TIMESTAMP NOT NULL,
+    CONSTRAINT user_date_time UNIQUE (user_id, date_time),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (restoration_id) REFERENCES restoration (id)
+);
